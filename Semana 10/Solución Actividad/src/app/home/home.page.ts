1fc61c4697105e19fc4fab001f7497da6f391b40
Ariel Silva <redthelegendarytrainer@gmail.com>
2023-06-16 18:14:13 -0400

    Solución

--- Semana 10/Solución Actividad/src/app/home/home.page.ts
+++ Semana 10/Solución Actividad/src/app/home/home.page.ts
@@ -8,12 +8,14 @@ import { firstValueFrom } from 'rxjs';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  modalPostear: boolean = false;
+
+  modalPublicacion: boolean = false;
   postData: any = {
     user: '',
+    icon: undefined,
     title: '',
     content: '',
-    attachments: undefined
+    attachments: []
   }
   publicaciones: any = [];
 
@@ -25,38 +27,44 @@ export class HomePage implements OnInit {
 
   async getData() {
     const publicaciones: any = await firstValueFrom(this._http.get('https://citt2023.up.railway.app/twitter'));
-    for (let publicacion of publicaciones) {
-      const fecha = new Date(publicacion.date);
-      const dia = fecha.getUTCDate();
-      const mesNumero = fecha.getUTCMonth();
+    for (let publcacion of publicaciones) {
+      const fecha = new Date(publcacion.date);
+      const diaNumero = fecha.getUTCDate();
+      const dia = diaNumero < 10 ? `0${diaNumero}` : diaNumero.toString();
+      const mesNumero = fecha.getUTCMonth() + 1;
       const mes = mesNumero < 10 ? `0${mesNumero}` : mesNumero.toString();
       const año = fecha.getUTCFullYear();
-      publicacion.formattedDate = `${dia}/${mes}/${año}`;
+      publcacion.formattedDate = `${dia}/${mes}/${año}`;
     }
     let now: any = new Date();
     this.publicaciones = publicaciones;
-    this.publicaciones.sort((valor1: any, valor2: any) => {
-      const diferenciaValor1 = Math.abs(new Date(valor1.date) as any - now);
-      const diferenciaValor2 = Math.abs(new Date(valor2.date) as any - now);
-      return diferenciaValor1 - diferenciaValor2;
+    this.publicaciones.sort((fecha1: any, fecha2: any) => {
+      const diferenciaFecha1 = Math.abs(new Date(fecha1.date) as any - now);
+      const diferenciaFecha2 = Math.abs(new Date(fecha2.date) as any - now);
+      return diferenciaFecha1 - diferenciaFecha2;
     });
-    console.log(this.publicaciones)
+    console.log(this.publicaciones);
   }
 
   async crearPost() {
-    const response = await firstValueFrom(this._http.post('https://citt2023.up.railway.app/twitter', this.postData))
+    const response = await firstValueFrom(this._http.post('https://citt2023.up.railway.app/twitter', this.postData));
     console.log(response);
-    this.postData = { user: '', title: '', content: '', attachments: undefined };
-    this.modalPostear = false;
+    this.postData = { user: '', icon: undefined, title: '', content: '', attachments: [] }
+    this.modalPublicacion = false;
   }
 
-  async fileChange(e: any) {
-    const b64Images = [];
-    for await (let file of e.target.files) {
-      let b64 = await this.getBase64(file);
-      b64Images.push(b64);
+  async onImageChange(e: any, type: string) {
+    if (type === 'icon') {
+      let image = e.target.files[0];
+      let b64 = await this.getBase64(image);
+      this.postData.icon = b64;
+    } else if (type === 'attachments') {
+      let images = e.target.files;
+      for await (let image of images) {
+        let b64 = await this.getBase64(image);
+        this.postData.attachments.push(b64);
+      }
     }
-    this.postData.attachments = b64Images;
   }
 
   getBase64(file: File) {
